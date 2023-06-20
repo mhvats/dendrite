3b3c31204674617df4be1c0c34cdb1d001ede1e1
Mehul <mehulwork11@gmail.com>
2023-06-20 22:42:35 +0530

    sidebarcomps are not able to change state individually

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,31 +1,62 @@
-import * as React from 'react';
-import './Sidebar.css'
-import {SidebarComps} from './SidebarComps'
-interface Props {
-    name ?: string;
-}
+import * as React from "react";
+import "./Sidebar.css";
+import { SidebarComps } from "./SidebarComps";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
 
-export const Sidebar: React.FunctionComponent<Props> = (props) => {
-    const handleHome = ()=>{
-        console.log("This is Home");
-    }
-    const handleSearch = ()=>{
-        console.log("This is search");
-    }
-    const handleFav = ()=>{
-        console.log("This is favourtie");
-    }
-    const handlePlaylist = ()=>{
-        console.log("This is playlists");
-    }
+export const Sidebar: React.FunctionComponent = () => {
+  //   const [iconContainer, setIconContainer] = React.useState<string>("menu-icon");
+  //   const [titleContainer, setTitleContainer] =
+  //     React.useState<string>("menu-title");
+  const handleHome = () => {
+    console.log("home clicked");
+  };
+  const handleSearch = () => {
+    console.log("search clicked");
+  };
+  const handleFav = () => {
+    console.log("fav clicked");
+  };
+  const handlePlaylist = () => {
+    console.log("playlist clicked");
+  };
   return (
     <>
-    <div>
-       <SidebarComps title='Home' onClick ={handleHome}/>
-       <SidebarComps title='Search' onClick = {handleSearch}/>
-       <SidebarComps title='Favourites' onClick = {handleFav}/>
-       <SidebarComps title='Playlists' onClick = {handlePlaylist}/>
-    </div>
+      <div
+        className="text-white d-flex flex-column w-25 justify-content-center"
+        style={{ height: "100vh", backgroundColor: "#5b3ede" }}
+      >
+        <SidebarComps
+          title="Home"
+          onClick={handleHome}
+          Icon={MenuIcon}
+          //   iconContainer={iconContainer}
+          //   titleContainer={titleContainer}
+        />
+        <SidebarComps
+          title="Search"
+          onClick={handleSearch}
+          Icon={SearchIcon}
+          //   iconContainer={iconContainer}
+          //   titleContainer={titleContainer}
+        />
+        <SidebarComps
+          title="Favourites"
+          onClick={handleFav}
+          Icon={FavoriteIcon}
+          //   iconContainer={iconContainer}
+          //   titleContainer={titleContainer}
+        />
+        <SidebarComps
+          title="Playlists"
+          onClick={handlePlaylist}
+          Icon={PlayCircleFilledWhiteIcon}
+          //   iconContainer={iconContainer}
+          //   titleContainer={titleContainer}
+        />
+      </div>
     </>
   );
 };
--- src/components/sidebar/SidebarComps.tsx
+++ src/components/sidebar/SidebarComps.tsx
@@ -1,20 +1,36 @@
-import * as React from 'react';
-
+import * as React from "react";
+import "./SidebarComps.css";
 interface Props {
-    title : string;
-    onClick : ()=> void;
+  title: string;
+  Icon?: React.ElementType;
+  onClick: () => void;
 }
 
-export const SidebarComps: React.FunctionComponent<Props> = ({title, onClick}) => {
-    const handleClick = ()=>{
-        onClick();
-    }
+export const SidebarComps: React.FunctionComponent<Props> = ({
+  title,
+  onClick,
+  Icon,
+}) => {
+  const handleClick = () => {
+    onClick();
+  };
   return (
     <>
-    <div>
-    <h1 onClick={handleClick} style={{cursor:"pointer"}}>{title}</h1>
-    </div>
+      <div className="d-flex align-items-center my-2 menu-container">
+        <div className=" px-5" style={{ cursor: "pointer" }}>
+          {Icon && <Icon />}
+        </div>
+        <div>
+          <h2
+            className=""
+            id="menu-title menu-title2"
+            onClick={handleClick}
+            style={{ cursor: "pointer" }}
+          >
+            {title}
+          </h2>
+        </div>
+      </div>
     </>
   );
 };
-
